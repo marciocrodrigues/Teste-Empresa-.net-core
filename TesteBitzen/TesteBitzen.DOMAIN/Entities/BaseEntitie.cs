a5e38b06517c3e50f244405f3d2350fd9b2566ca
Marcio Costa Rodrigues <marciodeath@hotmail.com>
2020-08-28 16:33:48 -0300

    Commit inicial

---
+++ TesteBitzen/TesteBitzen.DOMAIN/Entities/BaseEntitie.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TesteBitzen.DOMAIN.Entities
+{
+    public abstract class BaseEntitie
+    {
+        protected BaseEntitie()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        public Guid Id { get; set; }
+    }
+}
---
+++ TesteBitzen/TesteBitzen.TESTS/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TesteBitzen.TESTS
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
